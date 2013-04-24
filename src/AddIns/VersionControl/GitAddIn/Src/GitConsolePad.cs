cb327ce91d8395eeedddb2ccaab35a5162a8d737
Andreas Weizel <rpinski@awzhome.de>
2013-04-25 01:45:20 +0200

    Added dummy skeleton of a GitConsolePad.

---
+++ src/AddIns/VersionControl/GitAddIn/Src/GitConsolePad.cs
@@ -0,0 +1,32 @@
+﻿// Copyright (c) AlphaSierraPapa for the SharpDevelop Team (for details please see \doc\copyright.txt)
+// This code is distributed under the GNU LGPL (for details please see \doc\license.txt)
+using System;
+using ICSharpCode.Core;
+using ICSharpCode.SharpDevelop.Gui;
+
+namespace ICSharpCode.GitAddIn
+{
+	/// <summary>
+	/// Class representing the Git Console pad.
+	/// </summary>
+	public class GitConsolePad : AbstractConsolePad
+	{
+		public GitConsolePad()
+		{
+		}
+		
+		protected override string Prompt
+		{
+			get
+			{
+				return "> git ";
+			}
+		}
+		
+		protected override bool AcceptCommand(string command)
+		{
+			Append(Environment.NewLine + "executed command '" + command + "'");
+			return true;
+		}
+	}
+}
